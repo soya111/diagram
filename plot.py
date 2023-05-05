1062da2d5b9a39c533ee3c777b77c94cc8cd29cb
soya111 <soyak20000316@gmail.com>
2023-05-05 21:02:45 +0900

    Add function to estimate passing times for passing stations

--- plot.py
+++ plot.py
@@ -1,9 +1,11 @@
 import glob
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+import numpy as np
 
 from bokeh.models import CustomJSTickFormatter, FixedTicker
 from bokeh.plotting import figure, output_file, show
+
 from tokaido import get_train_color
 
 # データの読み込み
@@ -25,16 +27,81 @@ p = figure(
 )
 
 
-train_data_list = []
+def load_train_data_from_files():
+    """
+    JSON ファイルから列車データを読み込む
 
-# "trains/{train_name}.json" というパターンに一致するファイル名のリストを取得
-file_list = glob.glob("trains/*.json")
+    Returns:
+        list: 列車データのリスト
+    """
 
-# ファイルリストをループして、すべてのファイルを読み込み、JSON をパースし、リストに追加
-for file_name in file_list:
-    with open(file_name, "r", encoding="utf-8") as f:
-        train_data = json.load(f)
-        train_data_list.append(train_data)
+    train_data_list = []
+
+    # "trains/{train_name}.json" というパターンに一致するファイル名のリストを取得
+    file_list = glob.glob("trains/*.json")
+
+    # ファイルリストをループして、すべてのファイルを読み込み、JSON をパースし、リストに追加
+    for file_name in file_list:
+        with open(file_name, "r", encoding="utf-8") as f:
+            train_data = json.load(f)
+            train_data_list.append(train_data)
+
+    return train_data_list
+
+
+train_data_list = load_train_data_from_files()
+
+
+BASE_DATETIME = datetime(1900, 1, 1)
+
+
+def calculate_passing_times(time_data, station_indices):
+    """
+    通過駅の推定通過時刻を計算し、対応する time_data の位置に追加する関数。
+
+    引数:
+        time_data (list): 各駅の時刻データ (通過駅の場合は None) を格納したリスト。
+        station_indices (list): 各駅のインデックスデータを格納したリスト。
+
+    戻り値:
+        なし。time_data が更新され、通過駅の推定通過時刻が追加されます。
+    """
+
+    # 通過駅の推定通過時刻を計算
+
+    # まず、通過駅ではない駅の時刻データをUNIXタイムスタンプに変換し、time_data_np として保存します。
+    time_data_np = np.array(
+        [(x - BASE_DATETIME).total_seconds() for x in time_data if x is not None]
+    )
+
+    # 通過駅ではない駅のインデックスを station_indices_np として保存します。
+    station_indices_np = np.array(
+        [y for x, y in zip(time_data, station_indices) if x is not None]
+    )
+
+    # time_data から通過駅の位置とインデックスを取得し、passing_stations に保存します。
+    passing_stations = [
+        (i, idx)
+        for i, (x, idx) in enumerate(zip(time_data, station_indices))
+        if x is None
+    ]
+
+    # 通過駅が存在する場合
+    if len(passing_stations) > 0:
+        # 線形補完を使用して、通過駅の推定通過時刻を計算します。
+        # passing_stations のインデックス部分だけを渡して線形補完を行います。
+        estimated_passing_times = np.interp(
+            [idx for _, idx in passing_stations], station_indices_np, time_data_np
+        )
+
+        # 通過駅の推定通過時刻を、対応する time_data の位置に追加します。
+        # passing_stations に保存された位置情報 (i) を使用して、time_data に推定通過時刻を追加します。
+        for (i, _), passing_time in zip(passing_stations, estimated_passing_times):
+            time_data[i] = BASE_DATETIME + timedelta(seconds=passing_time)
+
+
+x_data_list = []
+y_data_list = []
 
 for train in train_data_list:
     x_data = []
@@ -61,7 +128,8 @@ for train in train_data_list:
 
         # 通過駅の場合
         if station["arrival"] == "" and station["departure"] == "":
-            pass
+            x_data.append(None)
+            y_data.append(station_indices[i])
         # 停車駅の場合
         else:
             if station["arrival"] != "":
@@ -71,6 +139,13 @@ for train in train_data_list:
                 x_data.append(datetime.strptime(station["departure"], "%H%M%S"))
                 y_data.append(station_indices[i])
 
+    # 通過駅の推定通過時刻を計算
+    calculate_passing_times(x_data, y_data)
+
+    x_data_list.append(x_data)
+    y_data_list.append(y_data)
+
+for x_data, y_data, train in zip(x_data_list, y_data_list, train_data_list):
     train_color = get_train_color(train["train_name"])
 
     # プロット
@@ -82,18 +157,18 @@ for train in train_data_list:
         text_font_size="10pt",
         text_color=train_color,
     )
-    p.circle(
-        x_data,
-        y_data,
-        size=5,
-        fill_color=train_color,
-        line_color=train_color,
-        alpha=0.5,
-    )
+    # p.circle(
+    #     x_data,
+    #     y_data,
+    #     size=5,
+    #     fill_color=train_color,
+    #     line_color=train_color,
+    #     alpha=0.5,
+    # )
 
 
 # 出力ファイルの設定
-output_file("line_plot.html")
+output_file("index.html")
 
 
 # y軸の目盛り位置を設定
